df549de22e704b64da7f39b7ba3df4be376fecc1
Igor Intercont <masterintercont@gmail.com>
2015-04-14 20:11:10 -0300

    licao 1 completa

--- app/src/main/java/br/com/intercont/sunshine/app/MainActivity.java
+++ app/src/main/java/br/com/intercont/sunshine/app/MainActivity.java
@@ -8,6 +8,8 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -62,18 +64,53 @@ public class MainActivity extends ActionBarActivity {
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+            List<String> weekForecast = new ArrayList<String>();
+
+            weekForecast.add("Hoje, Sol 27º");
+            weekForecast.add("Segunda, Sol 25º");
+            weekForecast.add("Terça, Sol 29º");
+            weekForecast.add("Quarta, Nublado 24º");
+            weekForecast.add("Quinta, Chuva 22º");
+            weekForecast.add("Sexta, Sol 25º");
+            weekForecast.add("Sabado, Sol 25º");
+            weekForecast.add("Domingo, Sol 25º");
+            weekForecast.add("Segunda, Sol 25º");
+            weekForecast.add("Terça, Sol 25º");
+            weekForecast.add("Quarta, Sol 25º");
+            weekForecast.add("Quinta, Sol 25º");
+
+
             //criando um array de strings
-            String[] forecastArray = {
-                "Hoje, Sol 27º",
-                "Segunda, Sol 25º",
-                "Terça, Sol 29º",
-                "Quarta, Nublado 24º",
-                "Quinta, Chuva 22º",
-                "Sexta, Sol 25º",
-            };
-            //convertendo-o para um ArrayList
-            List<String> weekForecast = new ArrayList<String>(
-                    Arrays.asList(forecastArray));
+//            String[] forecastArray = {
+//                "Hoje, Sol 27º",
+//                "Segunda, Sol 25º",
+//                "Terça, Sol 29º",
+//                "Quarta, Nublado 24º",
+//                "Quinta, Chuva 22º",
+//                "Sexta, Sol 25º",
+//            };
+//            //convertendo-o para um ArrayList
+//            List<String> weekForecast = new ArrayList<String>(
+//                    Arrays.asList(forecastArray));
+
+            //Adaptador para popular a ListView por uma fonte de dados, que no caso, é o ArrayList Mocado
+            //ele requer uma série de parâmetros, como se pode ver pelos diferentes construtores dele
+            ArrayAdapter<String> mForecastAdapter = new ArrayAdapter<String>(
+                    //o contexto corrente, ou seja, o pai deste fragment, a Activity
+                    getActivity(),
+                    //referência ao layout gráfico como um todoo, por isso o nome do arquivo xml
+                    R.layout.list_item_forecast,
+                    //referência ao elemento do TextView, dentro do arquivo de layout de antes
+                    R.id.list_item_forecast_textview,
+                    //fonte de dados, neste caso o ArrayList acima
+                    weekForecast);
+
+            //Trazendo o ListView pra cá, precisa trazer do rootView já que é onde está o ListView,
+            //o rootView inflou o fragment_main aí encima e tem todos seus elementos
+            ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
+            //e set o Adapter
+            listView.setAdapter(mForecastAdapter);
 
             return rootView;
         }
