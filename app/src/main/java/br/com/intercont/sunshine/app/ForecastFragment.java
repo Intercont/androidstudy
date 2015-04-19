b626cc1f3ec6feb48ba8829b66832ff3d95dcc6b
Intercont <intercont1@hotmail.com>
2015-04-19 17:44:27 -0300

    ForecastFragment sendo separado

--- app/src/main/java/br/com/intercont/sunshine/app/ForecastFragment.java
+++ app/src/main/java/br/com/intercont/sunshine/app/ForecastFragment.java
@@ -1,12 +1,10 @@
 package br.com.intercont.sunshine.app;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -21,50 +19,13 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Created by intercont on 19/04/15.
+ */
+public class ForecastFragment {
 
-public class MainActivity extends ActionBarActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new ForecastFragment())
-                    .commit();
+        public ForecastFragment() {
         }
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class ForecastFragment extends Fragment {
-
-        /*public PlaceholderFragment() {
-        }*/
 
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -180,5 +141,8 @@ public class MainActivity extends ActionBarActivity {
 
             return rootView;
         }
+
+    public class FetchWeatherTask extends AsyncTask{
+
     }
 }
