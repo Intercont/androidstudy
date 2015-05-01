ad4fe37fa18ce53863317b253008c84e6b2bd006
Igor Intercont <masterintercont@gmail.com>
2015-04-30 23:26:37 -0300

    Classe para Receber o JSON e extrair a temperatura maxima

---
+++ app/src/main/java/br/com/intercont/sunshine/app/WeatherDataParser.java
@@ -0,0 +1,28 @@
+package br.com.intercont.sunshine.app;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * Created by intercont on 29/04/15.
+ */
+public class WeatherDataParser {
+
+    /**
+     * Given a string of the form returned by the api call:
+     * http://api.openweathermap.org/data/2.5/forecast/daily?q=94043&mode=json&units=metric&cnt=7
+     * retrieve the maximum temperature for the day indicated by dayIndex
+     * (Note: 0-indexed, so 0 would refer to the first day).
+     */
+    public static double getMaxTemperatureForDay(String weatherJsonStr, int dayIndex)
+            throws JSONException {
+        // TODO: add parsing code here
+        JSONObject jsonObject = new JSONObject(weatherJsonStr);
+        JSONArray forecast = jsonObject.getJSONArray("list");
+        JSONObject day = forecast.getJSONObject(dayIndex);
+        JSONObject temps = day.getJSONObject("temp");
+        double maxTemp = temps.getDouble("max");
+        return maxTemp;
+    }
+
+}
