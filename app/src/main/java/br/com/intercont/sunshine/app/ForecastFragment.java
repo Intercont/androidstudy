c7f4aebeea4a54f0235950da19074763f9fc6cc2
Intercont <intercont1@hotmail.com>
2015-05-03 18:12:49 -0300

    Atualizando Interface com Dados do Backend via onPostExecute

--- app/src/main/java/br/com/intercont/sunshine/app/ForecastFragment.java
+++ app/src/main/java/br/com/intercont/sunshine/app/ForecastFragment.java
@@ -4,6 +4,7 @@ import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.format.Time;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -14,12 +15,17 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -27,6 +33,9 @@ import java.util.List;
  * Created by intercont on 19/04/15.
  */
 public class ForecastFragment extends Fragment {
+
+    private ArrayAdapter<String> mForecastAdapter;
+    private ListView listView;
 
     public ForecastFragment() {
     }
@@ -100,7 +109,7 @@ public class ForecastFragment extends Fragment {
 
         //Adaptador para popular a ListView por uma fonte de dados, que no caso, é o ArrayList Mocado
         //ele requer uma série de parâmetros, como se pode ver pelos diferentes construtores dele
-        ArrayAdapter<String> mForecastAdapter = new ArrayAdapter<String>(
+        mForecastAdapter = new ArrayAdapter<String>(
                 //o contexto corrente, ou seja, o pai deste fragment, a Activity
                 getActivity(),
                 //referência ao layout gráfico como um todoo, por isso o nome do arquivo xml
@@ -112,7 +121,7 @@ public class ForecastFragment extends Fragment {
 
         //Trazendo o ListView pra cá, precisa trazer do rootView já que é onde está o ListView,
         //o rootView inflou o fragment_main aí encima e tem todos seus elementos
-        ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
+        listView = (ListView) rootView.findViewById(R.id.listview_forecast);
         //e set o Adapter
         listView.setAdapter(mForecastAdapter);
         return rootView;
@@ -122,13 +131,13 @@ public class ForecastFragment extends Fragment {
      * FetchWeatherTask
      * Subclasse para execuçao da consulta na API dos dados em Background
      */
-    private class FetchWeatherTask extends AsyncTask<String, Void, Void> {
+    private class FetchWeatherTask extends AsyncTask<String, Void, String[]> {
 
         //TAG Usada para identificaçao do nome da classe no Logger, facilita para a identificaçao de erros e estao em sincronia caso refatore a classe
         private final String LOG_TAG = FetchWeatherTask.class.getSimpleName();
 
         @Override
-        protected Void doInBackground(String... postalcode) {
+        protected String[] doInBackground(String... postalcode) {
             //Trazendo dados reais da API do OpenWeather, comentários reais mantidos do Github
             // These two need to be declared outside the try/catch
             // so that they can be closed in the finally block.
@@ -177,10 +186,10 @@ public class ForecastFragment extends Fragment {
                         .appendQueryParameter(FORMAT_PARAM, format)
                         .appendQueryParameter(UNITS_PARAM, units)
                         .appendQueryParameter(DAYS_PARAM, Integer.toString(numDays))
-                        .appendQueryParameter(LANG_PARAM,lang);
+                        .appendQueryParameter(LANG_PARAM, lang);
 
                 String urlQuery = builder.build().toString();
-                Log.v("Query do Builder",urlQuery);
+//                Log.v("Query do Builder", urlQuery);
 
                 URL url = new URL(urlQuery);
                 // Create the request to OpenWeatherMap, and open the connection
@@ -212,7 +221,7 @@ public class ForecastFragment extends Fragment {
                 forecastJsonStr = buffer.toString();
 
                 //logando o retorno do backend da API do Tempo
-                Log.v(LOG_TAG, "String JSON da Previsao: " + forecastJsonStr);
+//                Log.v(LOG_TAG, "String JSON da Previsao: " + forecastJsonStr);
 
             } catch (IOException e) {
                 Log.e(LOG_TAG, "Error ", e);
@@ -232,7 +241,147 @@ public class ForecastFragment extends Fragment {
                 }
             }
 
-            return null;
+            String[] forecast = new String[]{};
+
+            try {
+                forecast = getWeatherDataFromJson(forecastJsonStr,numDays);
+            } catch (JSONException e) {
+                e.printStackTrace();
+                Log.e(LOG_TAG, "Error getting Data from JSON", e);
+            }
+
+            return forecast;
+        }
+
+         /* The date/time conversion code is going to be moved outside the asynctask later,
+         * so for convenience we're breaking it out into its own method now.
+         */
+        private String getReadableDateString(long time){
+            // Because the API returns a unix timestamp (measured in seconds),
+            // it must be converted to milliseconds in order to be converted to valid date.
+            SimpleDateFormat shortenedDateFormat = new SimpleDateFormat("EEE MMM dd");
+            return shortenedDateFormat.format(time);
+        }
+
+        /**
+         * Prepare the weather high/lows for presentation.
+         */
+        private String formatHighLows(double high, double low) {
+            // For presentation, assume the user doesn't care about tenths of a degree.
+            long roundedHigh = Math.round(high);
+            long roundedLow = Math.round(low);
+
+            String highLowStr = roundedHigh + "/" + roundedLow;
+            return highLowStr;
+        }
+
+        /**
+         * Take the String representing the complete forecast in JSON Format and
+         * pull out the data we need to construct the Strings needed for the wireframes.
+         *
+         * Fortunately parsing is easy:  constructor takes the JSON string and converts it
+         * into an Object hierarchy for us.
+         */
+        private String[] getWeatherDataFromJson(String forecastJsonStr, int numDays)
+                throws JSONException {
+
+            // These are the names of the JSON objects that need to be extracted.
+            final String OWM_LIST = "list";
+            final String OWM_WEATHER = "weather";
+            final String OWM_TEMPERATURE = "temp";
+            final String OWM_MAX = "max";
+            final String OWM_MIN = "min";
+            final String OWM_DESCRIPTION = "main";
+
+            JSONObject forecastJson = new JSONObject(forecastJsonStr);
+            JSONArray weatherArray = forecastJson.getJSONArray(OWM_LIST);
+
+            // OWM returns daily forecasts based upon the local time of the city that is being
+            // asked for, which means that we need to know the GMT offset to translate this data
+            // properly.
+
+            // Since this data is also sent in-order and the first day is always the
+            // current day, we're going to take advantage of that to get a nice
+            // normalized UTC date for all of our weather.
+
+            Time dayTime = new Time();
+            dayTime.setToNow();
+
+            // we start at the day returned by local time. Otherwise this is a mess.
+            int julianStartDay = Time.getJulianDay(System.currentTimeMillis(), dayTime.gmtoff);
+
+            // now we work exclusively in UTC
+            dayTime = new Time();
+
+            String[] resultStrs = new String[numDays];
+            for(int i = 0; i < weatherArray.length(); i++) {
+                // For now, using the format "Day, description, hi/low"
+                String day;
+                String description;
+                String highAndLow;
+
+                // Get the JSON object representing the day
+                JSONObject dayForecast = weatherArray.getJSONObject(i);
+
+                // The date/time is returned as a long.  We need to convert that
+                // into something human-readable, since most people won't read "1400356800" as
+                // "this saturday".
+                long dateTime;
+                // Cheating to convert this to UTC time, which is what we want anyhow
+                dateTime = dayTime.setJulianDay(julianStartDay+i);
+                day = getReadableDateString(dateTime);
+
+                // description is in a child array called "weather", which is 1 element long.
+                JSONObject weatherObject = dayForecast.getJSONArray(OWM_WEATHER).getJSONObject(0);
+                description = weatherObject.getString(OWM_DESCRIPTION);
+
+                // Temperatures are in a child object called "temp".  Try not to name variables
+                // "temp" when working with temperature.  It confuses everybody.
+                JSONObject temperatureObject = dayForecast.getJSONObject(OWM_TEMPERATURE);
+                double high = temperatureObject.getDouble(OWM_MAX);
+                double low = temperatureObject.getDouble(OWM_MIN);
+
+                highAndLow = formatHighLows(high, low);
+                resultStrs[i] = day + " - " + description + " - " + highAndLow;
+            }
+
+//            for (String s : resultStrs) {
+//                Log.v(LOG_TAG, "Forecast entry: " + s);
+//            }
+
+            return resultStrs;
+        }
+
+        @Override
+        protected void onPostExecute(String[] strings) {
+            super.onPostExecute(strings);
+
+            //SOLUÇÃO DO CURSO - LIMPA O OBJETO E O REALIMENTA COM UMA ITERAÇÃO - solução mais limpa
+            //COM ESTE MÉTODO, NÃO HÁ NECESSIDADE DE MOVER A listView PARA SER GLOBAL
+            if(strings != null){
+                mForecastAdapter.clear();
+                for(String dayForecastStr : strings){
+                    mForecastAdapter.add(dayForecastStr);
+                }
+            }
+            //SOLUÇÃO DO CURSO
+
+            //MINHA SOLUÇÃO - RECRIO O OBJETO E NOTIFICO DA MUDANÇA
+            //alimentar o Adapter aqui dentro com o array de dados que vem do doInBackground
+//            mForecastAdapter = new ArrayAdapter<String>(
+//                    //o contexto corrente, ou seja, o pai deste fragment, a Activity
+//                    getActivity(),
+//                    //referência ao layout gráfico como um todoo, por isso o nome do arquivo xml
+//                    R.layout.list_item_forecast,
+//                    //referência ao elemento do TextView, dentro do arquivo de layout de antes
+//                    R.id.list_item_forecast_textview,
+//                    //fonte de dados, neste caso o ArrayList acima
+//                    strings);
+//
+//            mForecastAdapter.notifyDataSetChanged();
+//            listView.setAdapter(mForecastAdapter);
+            //MINHA SOLUÇÃO
+
         }
     }
 }
